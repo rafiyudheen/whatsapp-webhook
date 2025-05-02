08e854754d2dc16b3a19c14434d28a5019fd0837
rafiyudheen <mrafiyudheen@gmail.com>
2025-05-03 02:09:47 +0530

    6 commit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/webhook", (req, res) => {
 
 // Receive messages (POST)
 app.post("/webhook", (req, res) => {
-  console.log(req.body.entry[0].changes[0].contacts.profile.name);
+  console.log(req.body.entry[0].changes[0].contacts[0].profile.name);
   console.log(req.body.entry[0].messages[0].from);
   console.log(req.body.entry[0].messages[0].timestamp);
   console.log(req.body.entry[0].messages[0].text.body);
